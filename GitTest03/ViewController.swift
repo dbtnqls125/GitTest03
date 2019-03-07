dae6f9adf07470b595d56ff85a5feaa58e161bfb
D7703_17 <d7703_17@d7703-17ui-imac.local>
2019-03-07 12:41:41 +0900

    change blue color and add comment

--- GitTest03/ViewController.swift
+++ GitTest03/ViewController.swift
@@ -13,9 +13,10 @@ class ViewController: UIViewController {
       override func viewDidLoad() {
             super.viewDidLoad()
             // Do any additional setup after loading the view, typically from a nib.
-            self.view.backgroundColor = UIColor.yellow
+            
+            // UIView의 background Color를 blue로 변경
+            self.view.backgroundColor = UIColor.blue
       }
-
 
 }
 
